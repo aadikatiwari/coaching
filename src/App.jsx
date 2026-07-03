490bf97b0428220942e502b5d608e69dad314324
aadikatiwari <aadikatiwari15@gmail.com>
2026-07-03 22:08:09 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Navbar from "./Components/Navbar";
+import HeroSlider from "./Components/HeroSlider";
+import Course from "./Components/Course";
 
 function App() {
   return (
     <>
-      <Navbar />
+      <Navbar/>
+      <HeroSlider/>
+      <Course/>
     </>
   );
 }
